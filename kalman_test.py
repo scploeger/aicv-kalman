985e85e56d1751ea28b3a2b173ec5b7a73cc2705
spencer <spenceinvents@hotmail.com>
2020-06-01 19:38:10 -0400

    modified kalman_test.py

--- kalman_test.py
+++ kalman_test.py
@@ -1,97 +1,63 @@
-'''
-By: Spencer Ploeger
-AICV Lab, University of Guelph
-
-Last updated: May 26, 2020
-
-Based off of example in: http://ros-developer.com/2019/04/10/kalman-filter-explained-with-python-code-from-scratch/
-
-About: A basic Kalman filter implemented in Python using numpy. Uses example position and acceleration data.
-'''
+#By: Spencer Ploeger
+#June 1 2020
+#Based off of examples in: https://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf
 
 import numpy as np
-np.set_printoptions(threshold=3)
-np.set_printoptions(suppress=True)
-from numpy import genfromtxt
+import matplotlib.pyplot as plt
 
-#notation
-#P - covariance matrix
-#F - prediciton matrix
-#B - control matrix
 
-def prediction(X_hat_t_1,P_t_1,F_t,B_t,U_t,Q_t):
-    X_hat_t=F_t.dot(X_hat_t_1)+(B_t.dot(U_t).reshape(B_t.shape[0],-1) )
-    P_t=np.diag(np.diag(F_t.dot(P_t_1).dot(F_t.transpose())))+Q_t
-    return X_hat_t,P_t
+iterations = 75
 
-def update(X_hat_t,P_t,Z_t,R_t,H_t):
-    K_prime=P_t.dot(H_t.transpose()).dot( np.linalg.inv ( H_t.dot(P_t).dot(H_t.transpose()) +R_t ) )  
-    print("K:\n",K_prime)
+#true value
+x = -2
+
+#create random noise for each "sample" with std. dev. of 0.2
+noise = np.random.normal(0, 0.2, size=iterations)
+
+A = -1 #negative so error converges
+H = 1 #=1 because the state IS the measurement
+
+Q = 0.0005
+R = 0.1 #assume a low measurement variance
+
+#empty lists
+x_hat = [0] * iterations
+x_hat_minus = [0] * iterations
+P = [0] * iterations
+P_minus = [0] * iterations
+K = [0] * iterations
+z = [0] * iterations
+
+
+#for first iteration assume all previous measurements are zero
+k = 0 #first iteration
+z[0] = x + noise[k] #measurement for this step is 
+
+#time update
+x_hat_minus[k] = 0
+P_minus[k] = 0 + Q
+
+#measurement update
+K[k] = P_minus[k]/(P_minus[k] + R)
+x_hat[k] = x_hat_minus[k] + K[k] * (z[0] - x_hat_minus[k])
+P[k] = (1 - K[k]) * P_minus[k]
+
+#for the rest of the iterations, loop
+for k in range(1, iterations):
+    z[k] = x + noise[k] #measurement for this step is 
+
+    #time update
+    x_hat_minus[k] = x_hat[k-1]
+    P_minus[k] = P[k-1] + Q
+
+    #measurement update
+    K[k] = P_minus[k]/(P_minus[k] + R)
+    x_hat[k] = x_hat_minus[k] + K[k] * (z[k] - x_hat_minus[k])
+    P[k] = (1 - K[k]) * P_minus[k]
     
-    X_t=X_hat_t+K_prime.dot(Z_t-H_t.dot(X_hat_t))
-    P_t=P_t-K_prime.dot(H_t).dot(P_t)
-    
-    return X_t,P_t
-
-acceleration=0
-delta_t=1/20#milisecond
- 
-groundTruth = genfromtxt('data/groundTruth.csv', delimiter=',',skip_header=1)
- 
-#Observations: position_X, position_Y
-measurments = genfromtxt('data/measurments.csv', delimiter=',',skip_header=1)
- 
-#Checking our result with OpenCV
-opencvKalmanOutput = genfromtxt('data/kalmanv.csv', delimiter=',',skip_header=1)
- 
-#Transition matrix
-F_t=np.array([ [1 ,0,delta_t,0] , [0,1,0,delta_t] , [0,0,1,0] , [0,0,0,1] ])
- 
-#Initial State cov
-P_t= np.identity(4)*0.2
- 
-#Process cov
-Q_t= np.identity(4)
- 
-#Control matrix
-B_t=np.array( [ [0] , [0], [0] , [0] ])
- 
-#Control vector
-U_t=acceleration
- 
-#Measurment Matrix
-H_t = np.array([ [1, 0, 0, 0], [ 0, 1, 0, 0]])
- 
-#Measurment cov
-R_t= np.identity(2)*5
-
-if __name__ == "__main__":
-    # Initial State
-    X_hat_t = np.array( [[0],[0],[0],[0]] )
-    print("X_hat_t",X_hat_t.shape)
-    print("P_t",P_t.shape)
-    print("F_t",F_t.shape)
-    print("B_t",B_t.shape)
-    print("Q_t",Q_t.shape)
-    print("R_t",R_t.shape)
-    print("H_t",H_t.shape)
-    
-    for i in range(measurments.shape[0]):
-        X_hat_t,P_hat_t = prediction(X_hat_t,P_t,F_t,B_t,U_t,Q_t)
-        print("Prediction:")
-        print("X_hat_t:\n",X_hat_t,"\nP_t:\n",P_t)
-        
-        Z_t=measurments[i].transpose()
-        Z_t=Z_t.reshape(Z_t.shape[0],-1)
-        
-        print(Z_t.shape)
-        
-        X_t,P_t=update(X_hat_t,P_hat_t,Z_t,R_t,H_t)
-        print("Update:")
-        print("X_t:\n",X_t,"\nP_t:\n",P_t)
-        X_hat_t=X_t
-        P_hat_t=P_t
-        
-        print("=========================================")
-        print("Opencv Kalman Output:")
-        print("X_t:\n",opencvKalmanOutput[i])
+plt.figure()
+plt.plot(x_hat, label = 'estimate', color = 'r')
+plt.plot(z, label = 'measurement', color = 'k')
+plt.axhline(y=x, label = 'ground truth')
+plt.legend()
+plt.show()
